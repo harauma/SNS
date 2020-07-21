e8f1d027c2bba970854b80be9d5a65ebfac0bb42
原涼馬 <fot39hara@gmail.com>
2020-07-22 03:17:57 +0900

    React/Go HotReload対応

--- api/main.go
+++ api/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/labstack/echo"
 )
 
 func main() {
-	// controller
-	http.HandleFunc("/", echoHello)
-	// port
-	http.ListenAndServe(":8000", nil)
+    e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello motio")
+    })
+    e.Logger.Fatal(e.Start(":8000"))
 }
 
 func echoHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello World</h1>")
+	fmt.Fprintf(w, "<h1>Hello motio</h1>")
 }
