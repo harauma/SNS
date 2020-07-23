c57972599c32b9d601496d2822e460472183a26e
原涼馬 <fot39hara@gmail.com>
2020-07-23 23:51:10 +0900

    GET,POSTのAPIサンプル作成

--- api/main.go
+++ api/main.go
@@ -6,14 +6,40 @@ import (
 	"github.com/labstack/echo"
 )
 
+type User struct {
+    Name  string `json:"name"`
+    Age int `json:"age"`
+}
+
 func main() {
-    e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello motio")
-    })
+	e := echo.New()
+	initRouting(e)
     e.Logger.Fatal(e.Start(":8000"))
 }
 
-func echoHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello motio</h1>")
+func initRouting(e *echo.Echo) {
+	e.GET("/", echoHello)
+	e.GET("/user", getUser)
+	e.POST("/user", createUser)
+}
+
+func echoHello(c echo.Context) error {
+	fmt.Println("echoHelloが呼ばれました！")
+	return c.String(http.StatusOK, "Hello motio")
+}
+
+func getUser(c echo.Context) error {
+	fmt.Println("getUserが呼ばれました！")
+	name := c.QueryParam("name")
+    age := c.QueryParam("age")
+    return c.String(http.StatusOK, "name:" + name + ", age:" + age)
+}
+
+func createUser(c echo.Context) error {
+	fmt.Println("createUserが呼ばれました！")
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+        return err
+    }
+    return c.JSON(http.StatusOK, u)
 }
