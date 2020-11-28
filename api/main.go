64ac974a09394cf3354479023a47bfb17f231f5c
原涼馬 <fot39hara@gmail.com>
2020-11-28 17:20:06 +0900

    DBから全件取得するAPIサンプル作成

--- api/main.go
+++ api/main.go
@@ -4,11 +4,30 @@ import (
     "fmt"
     "net/http"
     "github.com/labstack/echo"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/jinzhu/gorm"
+)
+
+const (
+    // データベース
+    Dialect = "mysql"
+
+    // ユーザー名
+    DBUser = "sisupiyo"
+
+    // パスワード
+    DBPass = "sisupiyo"
+
+    // プロトコル
+    DBProtocol = "tcp(mysql:3306)"
+
+    // DB名
+    DBName = "test_db"
 )
 
 type User struct {
+    Id string `json:"id"`
     Name  string `json:"name"`
-    Age int `json:"age"`
 }
 
 func main() {
@@ -24,6 +43,18 @@ func initRouting(e *echo.Echo) {
     e.POST("/user", createUser)
 }
 
+func connectGorm() *gorm.DB {
+    connectTemplate := "%s:%s@%s/%s"
+    connect := fmt.Sprintf(connectTemplate, DBUser, DBPass, DBProtocol, DBName)
+    db, err := gorm.Open(Dialect, connect)
+
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+
+    return db
+}
+
 func echoHello(c echo.Context) error {
     fmt.Println("echoHelloが呼ばれました！")
     return c.String(http.StatusOK, "Hello motio")
@@ -31,9 +62,25 @@ func echoHello(c echo.Context) error {
 
 func getUsers(c echo.Context) error {
     fmt.Println("getUsersが呼ばれました！")
-    name := c.QueryParam("name")
-    age := c.QueryParam("age")
-    return c.String(http.StatusOK, "name:" + name + ", age:" + age)
+
+    db := connectGorm()
+    db.SingularTable(true)
+    defer db.Close()
+
+    result := findAll(db)
+    fmt.Println(result)
+
+    // name := c.QueryParam("name")
+    // age := c.QueryParam("age")
+
+    // return c.String(http.StatusOK, "name:" + name + ", age:" + age)
+    return c.JSON(http.StatusOK, result)
+}
+
+func findAll(db *gorm.DB) []User {
+    var allUsers []User
+    db.Find(&allUsers)
+    return allUsers
 }
 
 func getUser(c echo.Context) error {
