c95d7ee78dc76ed3615792393ffa778630211722
原涼馬 <fot39hara@gmail.com>
2020-11-28 13:51:48 +0900

    MySQLのデータ永続化できるようになった！(vagrant使用しない場合)

--- docker/mysql/initdb.d/user.sql
+++ docker/mysql/initdb.d/user.sql
@@ -1,14 +1,9 @@
-DROP SCHEMA IF EXISTS sample;
-CREATE SCHEMA sample;
-USE sample;
-
+USE test_db;
 DROP TABLE IF EXISTS user;
-
 CREATE TABLE user
 (
   id           INT(10),
   name     VARCHAR(40)
 );
-
 INSERT INTO user (id, name) VALUES (1, "Nagaoka");
 INSERT INTO user (id, name) VALUES (2, "Tanaka");
